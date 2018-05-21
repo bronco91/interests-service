d008f03a10d78c61cb706edc8dfac04910556590
EmpireX <empirex1991@gmail.com>
2018-05-21 18:23:12 -0400

    @ginoecc - First commit to test service infrastructure and platform

---
+++ src/main/java/io/ginoecc/interests/interestsservice/controller/InterestController.java
@@ -0,0 +1,50 @@
+package io.ginoecc.interests.interestsservice.controller;
+
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/dashboard")
+public class InterestController {
+
+    @GetMapping(path = "/test")
+    @ResponseBody
+    public TestResponse hello() {
+        return new TestResponse("Testing service's infrastructure and platform");
+    }
+
+    public static class TestResponse {
+
+        private String message;
+        private Date timeStamp;
+
+        public TestResponse(){
+
+        }
+
+        public TestResponse(String message){
+            this.message = message;
+            this.timeStamp = new Date();
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public Date getTimeStamp() {
+            return timeStamp;
+        }
+
+        public void setTimeStamp(Date timeStamp) {
+            this.timeStamp = timeStamp;
+        }
+    }
+}
---
+++ src/test/java/io/ginoecc/interests/interestsservice/InterestsServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package io.ginoecc.interests.interestsservice;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+public class InterestsServiceApplicationTests {
+
+	@Test
+	public void givenOneShouldReturnOne() {
+		assertThat(1).isEqualTo(1);
+	}
+
+}
